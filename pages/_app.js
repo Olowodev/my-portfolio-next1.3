41b0d3f2d5811174ed4fc16a7217de44c90fd389
Adebayo Nathaniel Olowofoyeku <99045599+Olowodev@users.noreply.github.com>
2023-10-18 14:15:34 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
 import { Curtains, useCurtains, useCurtainsEvent } from 'react-curtains'
 import Navbar from '../components/navbar/Navbar'
 import '../styles/globals.css'
-import styles from '../components/navbar/navbar.module.css'
-import {useRef} from 'react'
 // import '../components/_barba'
 
 function MyApp({ Component, pageProps }) {
-  const path2 = useRef(null)
   
   const contextLost = (curtains) => {
     console.log('lost')
@@ -17,10 +14,7 @@ function MyApp({ Component, pageProps }) {
     // <div data-barba='wrapper'>
     <>
       <Navbar />
-      <Component {...pageProps, path2} />
-    <svg className={styles.overlay2} width="100%" height="100%" viewBox="0 0 100 100" preserveAspectRatio="none">
-                    <path ref={path2} stroke='#222' fill='#222' className={styles.overlay__path2} vectorEffect="non-scaling-stroke" d="M 0 100 V 100 Q 50 100 100 100 V 100 z" />
-                </svg>
+      <Component {...pageProps} />
     </>
     // </div> 
     // </Curtains>
