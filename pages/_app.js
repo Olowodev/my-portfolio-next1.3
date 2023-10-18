fe69dec9da4ad5c4671a0016a555b837b291fe2e
Adebayo Nathaniel Olowofoyeku <99045599+Olowodev@users.noreply.github.com>
2023-10-18 13:57:15 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,9 @@ function MyApp({ Component, pageProps }) {
     <>
       <Navbar />
       <Component {...pageProps} />
+    <svg className={styles.overlay2} width="100%" height="100%" viewBox="0 0 100 100" preserveAspectRatio="none">
+                    <path ref={path2} stroke='#222' fill='#222' className={styles.overlay__path2} vectorEffect="non-scaling-stroke" d="M 0 100 V 100 Q 50 100 100 100 V 100 z" />
+                </svg>
     </>
     // </div> 
     // </Curtains>
